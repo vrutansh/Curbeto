10c7e9b6f2684e615b480aacae70f467c9bd1c36
vrutansh <vrutanshpatel01@gmail.com>
2023-11-03 21:56:16 +0530

    bugs

--- script.js
+++ script.js
@@ -3,25 +3,42 @@ Shery.makeMagnet(".magnet");
 Shery.hoverWithMediaCircle(".hvr", {videos:["./0.mp4", "./2.mp4", "./3.mp4" ]});
 
 gsap.to(".felfelem", {
-    scrollTrigger: {
-        trigger:"#fimages",
-        pin:true,
-        start:"top top",
-        end:"bottom bottom",
-        endTrigger:".last",
-        scrub: 1
-    },
-    y:"-300%",
-    ease: Power1
-})
+  scrollTrigger: {
+    trigger: "#fimages",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: Power1,
+});
+
+// let sections = document.querySelectorAll(".felfelem");
+// Shery.imageEffect(".images", {
+//   style: 4,
+//   config: { onMouse: { value: 1 } },
+//   slideStyle: (setScroll) => {
+//     sections.forEach(function (section, index) {
+//       ScrollTrigger.create({
+//         trigger: section,
+//         start: "top top",
+//         scrub: 1,
+//         onUpdate: function (prog) {
+//           setScroll(prog.progress + index);
+//         },
+//       });
+//     });
+//   },
+// });
 
 
-
-Shery.imageEffect(".images", {
-    style: 5,
-    slideStyle: (setScroll) => {
-      window.addEventListener("scroll", () => {
-        setScroll(window.scrollY / innerHeight); //Updating the scroll
-      });
-    },
-  });
+// Shery.imageEffect(".images", {
+//     style: 5,
+//     slideStyle: (setScroll) => {
+//       window.addEventListener("scroll", () => {
+//         setScroll(window.scrollY / innerHeight); //Updating the scroll
+//       });
+//     },
+//   });
