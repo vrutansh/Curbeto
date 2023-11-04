df2563045530a05128b098590c4d64df9973f637
vrutansh <vrutanshpatel01@gmail.com>
2023-11-04 14:55:18 +0530

    filler

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 Shery.mouseFollower();
 Shery.makeMagnet(".magnet");
 Shery.hoverWithMediaCircle(".hvr", {videos:["./0.mp4", "./2.mp4", "./3.mp4" ]});
-
+Shery.makeMagnet(".headings")
 gsap.to(".felfelem", {
   scrollTrigger: {
     trigger: "#fimages",
@@ -15,20 +15,20 @@ gsap.to(".felfelem", {
   ease: Power1
 });
 
-// let section = document.querySelectorAll(".felfelem");
-// Shery.imageEffect(".images", {
-//     style: 4,
-//     config: { onMouse: { value: 1 } },
-//     slideStyle: (setScroll) => {
-//       section.forEach(function(section, index){
-//         ScrollTrigger.create({
-//           trigger: section,
-//           start: "top top ",
-//           scrub: 1,
-//           onUpdate: function(prog){
-//             setScroll(prog.progress + index);
-//           },
-//         });
-//       });  
-//     },
-//   });
+let section = document.querySelectorAll(".felfelem");
+Shery.imageEffect(".images", {
+    style: 4,
+    config: { onMouse: { value: 1 } },
+    slideStyle: (setScroll) => {
+      section.forEach(function(section, index){
+        ScrollTrigger.create({
+          trigger: section,
+          start: "top top ",
+          scrub: 1,
+          onUpdate: function(prog){
+            setScroll(prog.progress + index);
+          },
+        });
+      });  
+    },
+  });
